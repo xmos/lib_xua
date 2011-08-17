b821b920415b3d3a4070e2b37c026078dc9dc153
Russell Gallop <russell@xmos.com>
2011-08-17 16:31:03 +0100

    Add read_byte_via_xc_ptr_indexed for reading from buffer byte by byte.

--- module_usb_aud_shared/usb_buffer/xc_ptr.h
+++ module_usb_aud_shared/usb_buffer/xc_ptr.h
@@ -17,6 +17,7 @@ inline xc_ptr array_to_xc_ptr(unsigned a[]) {
 #define read_via_xc_ptr(x,p)  asm("ldw %0, %1[0]":"=r"(x):"r"(p));
 
 #define read_via_xc_ptr_indexed(x,p,i)  asm("ldw %0, %1[%2]":"=r"(x):"r"(p),"r"(i));
+#define read_byte_via_xc_ptr_indexed(x,p,i)  asm("ld8u %0, %1[%2]":"=r"(x):"r"(p),"r"(i));
 
 #define GET_SHARED_GLOBAL(x, g) asm("ldw %0, dp[" #g "]":"=r"(x))
 #define SET_SHARED_GLOBAL(g, v) asm("stw %0, dp[" #g "]"::"r"(v))
