9a3b011c95ccbaef8c448dd0e5231d3cafab3392
Ross Owen <ross@xmos.com>
2011-10-21 14:40:42 +0100

    Added testmodes to ep0

--- module_usb_aud_shared/endpoint0/endpoint0.h
+++ module_usb_aud_shared/endpoint0/endpoint0.h
@@ -13,8 +13,10 @@
  *                   present
  *  \param c_clk_ctl Optional chanend to be connected to the clockgen thread if
  *                   present.
+ *  \param c_usb_test Optional chanend to be connected to XUD if test modes required.
  */
-void Endpoint0( chanend c_ep0_out, chanend c_ep0_in, chanend c_audioCtrl, chanend ?c_mix_ctl,chanend ?c_clk_ctl
+void Endpoint0( chanend c_ep0_out, chanend c_ep0_in, chanend c_audioCtrl, 
+        chanend ?c_mix_ctl,chanend ?c_clk_ctl, chanend ?c_usb_test
 #ifdef EP0_THREAD_COMBINED_WITH_SPI
   , chanend c_spi, chanend c_spi_ss
 #endif
