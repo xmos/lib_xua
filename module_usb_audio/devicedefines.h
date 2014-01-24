92087a2d40da2276c06dffd2b616532e65ea002d
Ross Owen <ross@xmos.com>
2014-01-24 16:50:25 +0000

    'Release: 6.4.0beta1'

--- module_usb_audio/devicedefines.h
+++ module_usb_audio/devicedefines.h
@@ -275,7 +275,7 @@
 /* Device release number in BCD: 0xJJMN */
 #define BCD_DEVICE_J             6
 #define BCD_DEVICE_M             4
-#define BCD_DEVICE_N             3
+#define BCD_DEVICE_N             0
 
 #ifndef BCD_DEVICE
 #define BCD_DEVICE               ((BCD_DEVICE_J << 8) | ((BCD_DEVICE_M & 0xF) << 4) | (BCD_DEVICE_N & 0xF))
