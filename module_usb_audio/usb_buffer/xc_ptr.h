ea5d579631f1aeb50ad81a0308448ac799425594
Ross Owen <ross@xmos.com>
2014-03-06 10:29:02 +0000

    Added macros for 16bit memory load/store to xc_ptr.h

--- module_usb_audio/usb_buffer/xc_ptr.h
+++ module_usb_audio/usb_buffer/xc_ptr.h
@@ -14,19 +14,25 @@ inline xc_ptr array_to_xc_ptr(const unsigned a[])
   return x;
 }
 
-#define write_via_xc_ptr(p,x)               asm volatile("stw %0, %1[0]"::"r"(x),"r"(p))
+#define write_via_xc_ptr(p,x)                   asm volatile("stw %0, %1[0]"::"r"(x),"r"(p))
 
-#define write_via_xc_ptr_indexed(p,i,x)     asm volatile("stw %0, %1[%2]"::"r"(x),"r"(p),"r"(i))
-#define write_byte_via_xc_ptr_indexed(p,i,x) asm volatile("st8 %0, %1[%2]"::"r"(x),"r"(p),"r"(i))
+#define write_via_xc_ptr_indexed(p,i,x)         asm volatile("stw %0, %1[%2]"::"r"(x),"r"(p),"r"(i))
+#define write_byte_via_xc_ptr_indexed(p,i,x)    asm volatile("st8 %0, %1[%2]"::"r"(x),"r"(p),"r"(i))
+#define write_byte_via_xc_ptr_indexed(p,i,x)    asm volatile("st8 %0, %1[%2]"::"r"(x),"r"(p),"r"(i))
+#define write_short_via_xc_ptr_indexed(p,i,x)   asm volatile("st16 %0, %1[%2]"::"r"(x),"r"(p),"r"(i))
 // No immediate st8 format
-#define write_byte_via_xc_ptr(p,x)          write_byte_via_xc_ptr_indexed(p, 0, x)
+#define write_byte_via_xc_ptr(p,x)              write_byte_via_xc_ptr_indexed(p, 0, x)
+#define write_short_via_xc_ptr(p,x)             write_short_via_xc_ptr_indexed(p, 0, x)
 
-#define read_via_xc_ptr(x,p)                asm("ldw %0, %1[0]":"=r"(x):"r"(p));
+#define read_via_xc_ptr(x,p)                    asm("ldw %0, %1[0]":"=r"(x):"r"(p));
 
-#define read_via_xc_ptr_indexed(x,p,i)      asm("ldw %0, %1[%2]":"=r"(x):"r"(p),"r"(i));
-#define read_byte_via_xc_ptr_indexed(x,p,i) asm("ld8u %0, %1[%2]":"=r"(x):"r"(p),"r"(i));
+#define read_via_xc_ptr_indexed(x,p,i)          asm("ldw %0, %1[%2]":"=r"(x):"r"(p),"r"(i));
+#define read_byte_via_xc_ptr_indexed(x,p,i)     asm("ld8u %0, %1[%2]":"=r"(x):"r"(p),"r"(i));
+#define read_short_via_xc_ptr_indexed(x,p,i)    asm("ld16s %0, %1[%2]":"=r"(x):"r"(p),"r"(i));
+
 // No immediate ld8u format
-#define read_byte_via_xc_ptr(x,p)  read_byte_via_xc_ptr_indexed(x, p, 0)
+#define read_byte_via_xc_ptr(x,p)               read_byte_via_xc_ptr_indexed(x, p, 0)
+#define read_short_via_xc_ptr(x,p)              read_short_via_xc_ptr_indexed(x, p, 0)
 
 #define GET_SHARED_GLOBAL(x, g) asm volatile("ldw %0, dp[" #g "]":"=r"(x)::"memory")
 #define SET_SHARED_GLOBAL(g, v) asm volatile("stw %0, dp[" #g "]"::"r"(v):"memory")
