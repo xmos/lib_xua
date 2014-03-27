85e48e619aefa3d2cea6ca979cedf2971d564214
Ross Owen <ross@xmos.com>
2014-03-27 11:31:49 +0000

    Removed old EP_NUM_ defines from descriptors.h

--- module_usb_audio/endpoint0/descriptors.h
+++ module_usb_audio/endpoint0/descriptors.h
@@ -630,22 +630,6 @@ enum USBInterfaceNumber
 #define ENDPOINT_ADDRESS_OUT_MIDI       (ENDPOINT_NUMBER_OUT_MIDI)
 #define ENDPOINT_ADDRESS_OUT_IAP        (ENDPOINT_NUMBER_OUT_IAP)
 
-#if 0
-#define EP_NUM_IN_FB             (1)     /* Always 1 */
-#define EP_NUM_IN_AUD            (2)     /* Always 2 */
-#define EP_NUM_IN_AUD_INT        (EP_NUM_IN_AUD + EP_CNT_IN_AUD_INT)     /* Audio interrupt/status EP */
-#define EP_NUM_IN_MIDI           (EP_NUM_IN_AUD_INT + 1)
-#define EP_NUM_IN_HID            (EP_NUM_IN_AUD_INT + EP_CNT_IN_MIDI + 1)
-#define EP_NUM_IN_IAP            (EP_NUM_IN_AUD_INT + EP_CNT_IN_MIDI + EP_CNT_IN_HID + 1) /* iAP Bulk */
-#ifdef IAP_INT_EP
-#define EP_NUM_IN_IAP_INT        (EP_NUM_IN_AUD_INT + EP_CNT_IN_MIDI + EP_CNT_IN_HID + 2) /* iAP interrupt */
-#endif
-
-#define EP_NUM_OUT_AUD           (1)     /* Always 1 */
-#define EP_NUM_OUT_MIDI          (2)     /* Always 2 */
-#define EP_NUM_OUT_IAP           (EP_NUM_OUT_AUD + EP_CNT_OUT_MIDI + 1)
-#endif
-
 /***** Device Descriptors *****/
 
 #if defined(AUDIO_CLASS_FALLBACK) || (AUDIO_CLASS==1)
@@ -1798,6 +1782,7 @@ USB_Config_Descriptor_Audio2_t cfgDesc_Audio2=
     64,                                /* 4  wMaxPacketSize */
     8,                                 /* 6  bInterval */
 #endif
+
 };
 #endif
 
