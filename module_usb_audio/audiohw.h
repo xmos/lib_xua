f8f500e8e6c0dbcbc9338c9c2ce3922d498a8fcd
Ross Owen <ross@xmos.com>
2014-04-10 11:57:43 +0100

    Added sample res params to AudioHwConfig() prototype

--- module_usb_audio/audiohw.h
+++ module_usb_audio/audiohw.h
@@ -3,12 +3,11 @@
 
 /* These functions must be implemented for the CODEC/ADC/DAC arrangement of a specific design */
 
-/* TODO Are the channel args required? */
-
 /* Any required clocking and CODEC initialisation - run once at start up */
 void AudioHwInit(chanend ?c_codec);
 
 /* Configure audio hardware (clocking, CODECs etc) for a specific mClk/Sample frquency - run on every sample frequency change */
-void AudioHwConfig(unsigned samFreq, unsigned mClk, chanend ?c_codec, unsigned dsdMode);
+void AudioHwConfig(unsigned samFreq, unsigned mClk, chanend ?c_codec, unsigned dsdMode, 
+        unsigned sampRes_DAC, unsigned sampRes_ADC);
 
 #endif
