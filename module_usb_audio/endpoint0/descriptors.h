03c3022ac4095fdadd0f107d6da2c0abf379dedb
Ross Owen <ross@xmos.com>
2015-04-07 14:58:14 +0100

    Changes to allow new descriptor_defs.h file to be included in XC

--- module_usb_audio/endpoint0/descriptors.h
+++ module_usb_audio/endpoint0/descriptors.h
@@ -7,10 +7,14 @@
 #ifndef _DEVICE_DESCRIPTORS_
 #define _DEVICE_DESCRIPTORS_
 
+#include <stddef.h>
 #include "devicedefines.h"          /* Device specific define */
+#include "descriptor_defs.h"
 #include "usbaudio20.h"             /* Defines from the USB Audio 2.0 Specifications */
 #include "usbaudiocommon.h"
 #include "usb_std_descriptors.h"
+#include "usbaudio20.h"          /* Defines from USB Audio 2.0 spec */
+#include "usb_defs.h"
 
 #ifdef IAP_EA_NATIVE_TRANS
 #include "iap2.h"                   /* Defines iAP EA Native Transport protocol name */
@@ -24,6 +28,7 @@
 
 #define STR_TABLE_ENTRY(name) char *name
 
+#if __STDC__
 typedef struct
 {
     STR_TABLE_ENTRY(langID);
@@ -368,60 +373,6 @@ StringDescTable_t g_strTable =
     .iAP_EANativeTransport_InterfaceStr = IAP2_EA_NATIVE_TRANS_PROTOCOL_NAME,
 #endif
 };
-
-/* Interface numbers enum */
-enum USBInterfaceNumber
-{
-    INTERFACE_NUMBER_AUDIO_CONTROL = 0,
-#if (NUM_USB_CHAN_OUT > 0)
-    INTERFACE_NUMBER_AUDIO_OUTPUT,
-#endif
-#if (NUM_USB_CHAN_IN > 0)
-    INTERFACE_NUMBER_AUDIO_INPUT,
-#endif
-#if defined(MIDI) && (MIDI != 0)
-    INTERFACE_NUMBER_MIDI_CONTROL,
-    INTERFACE_NUMBER_MIDI_STREAM,
-#endif
-#if defined(DFU) && (DFU != 0)
-    INTERFACE_NUMBER_DFU,
-#endif
-#if defined(IAP) && (IAP != 0)
-    INTERFACE_NUMBER_IAP,
-#if defined(IAP_EA_NATIVE_TRANS) && (IAP_EA_NATIVE_TRANS != 0)
-    INTERFACE_NUMBER_IAP_EA_NATIVE_TRANS,
-#endif
-#endif
-#if defined(HID_CONTROLS) && (HID_CONTROLS != 0)
-    INTERFACE_NUMBER_HID,
-#endif
-    INTERFACE_COUNT          /* End marker */
-};
-
-#if (NUM_USB_CHAN_IN > 0) && (NUM_USB_CHAN_OUT > 0)
-#define AUDIO_INTERFACE_COUNT 3
-#elif (NUM_USB_CHAN_IN > 0) || (NUM_USB_CHAN_OUT > 0)
-#define AUDIO_INTERFACE_COUNT 2
-#else
-#define AUDIO_INTERFACE_COUNT 1
-#endif
-
-/* Endpoint address defines */
-#define ENDPOINT_ADDRESS_IN_CONTROL               (ENDPOINT_NUMBER_IN_CONTROL | 0x80)
-#define ENDPOINT_ADDRESS_IN_FEEDBACK              (ENDPOINT_NUMBER_IN_FEEDBACK | 0x80)
-#define ENDPOINT_ADDRESS_IN_AUDIO                 (ENDPOINT_NUMBER_IN_AUDIO | 0x80)
-#define ENDPOINT_ADDRESS_IN_INTERRUPT             (ENDPOINT_NUMBER_IN_INTERRUPT | 0x80)
-#define ENDPOINT_ADDRESS_IN_MIDI                  (ENDPOINT_NUMBER_IN_MIDI | 0x80)
-#define ENDPOINT_ADDRESS_IN_HID                   (ENDPOINT_NUMBER_IN_HID | 0x80)
-#define ENDPOINT_ADDRESS_IN_IAP_INT               (ENDPOINT_NUMBER_IN_IAP_INT | 0x80)
-#define ENDPOINT_ADDRESS_IN_IAP                   (ENDPOINT_NUMBER_IN_IAP | 0x80)
-#define ENDPOINT_ADDRESS_IN_IAP_EA_NATIVE_TRANS   (ENDPOINT_NUMBER_IN_IAP_EA_NATIVE_TRANS | 0x80)
-
-#define ENDPOINT_ADDRESS_OUT_CONTROL              (ENDPOINT_NUMBER_OUT_CONTROL)
-#define ENDPOINT_ADDRESS_OUT_AUDIO                (ENDPOINT_NUMBER_OUT_AUDIO)
-#define ENDPOINT_ADDRESS_OUT_MIDI                 (ENDPOINT_NUMBER_OUT_MIDI)
-#define ENDPOINT_ADDRESS_OUT_IAP                  (ENDPOINT_NUMBER_OUT_IAP)
-#define ENDPOINT_ADDRESS_OUT_IAP_EA_NATIVE_TRANS  (ENDPOINT_NUMBER_OUT_IAP_EA_NATIVE_TRANS)
 
 /***** Device Descriptors *****/
 
@@ -2514,5 +2465,6 @@ unsigned char cfgDesc_Audio1[] =
 #endif
 };
 
+#endif
 #endif
 #endif
