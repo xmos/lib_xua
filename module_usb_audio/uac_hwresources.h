80604d2acb0c66c4eaeb70fcd5855a3984d1f593
Ross Owen <ross@xmos.com>
2015-05-22 11:18:29 +0100

    Added CLKBLK_ADAT_RX

--- module_usb_audio/uac_hwresources.h
+++ module_usb_audio/uac_hwresources.h
@@ -31,17 +31,14 @@
 
 #else
 
-/* XUD_U_SERIES */
-
+/* XUD_U_SERIES, XUD_X200_SERIES */
+/* Note, U-series XUD uses clock blocks 4 and 5 - see XUD_Ports.xc */
 #define CLKBLK_MIDI        XS1_CLKBLK_REF;
 #define CLKBLK_SPDIF_TX    XS1_CLKBLK_1
 #define CLKBLK_SPDIF_RX    XS1_CLKBLK_1
-#define CLKBLK_MCLK        XS1_CLKBLK_2 /* Note, potentially used twice */
-#define CLKBLK_FLASHLIB    XS1_CLKBLK_3 /* Clock block for use by flash lib */
-
-/* use REF for ADAT_RX on U-series */
-/* #define CLKBLK_ADAT_RX     XS1_CLKBLK_3 */
-/* Note, U-series XUD uses clock blocks 4 and 5 - see XUD_Ports.xc */
+#define CLKBLK_MCLK        XS1_CLKBLK_2   /* Note, potentially used twice */
+#define CLKBLK_FLASHLIB    XS1_CLKBLK_3   /* Clock block for use by flash lib */
+#define CLKBLK_ADAT_RX     XS1_CLKBLK_REF /* Use REF for ADAT_RX on U/x200 series */
 #define CLKBLK_I2S_BIT     XS1_CLKBLK_3
 #endif
 
