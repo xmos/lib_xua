cc14679d53aba8fd519ca5d90265cd6808befb76
Ross Owen <ross@xmos.com>
2015-09-04 12:56:14 +0100

    Fixed build issue due to undeclared var when DFU disabled.

--- module_usb_audio/endpoint0/endpoint0.c
+++ module_usb_audio/endpoint0/endpoint0.c
@@ -586,8 +586,12 @@ void Endpoint0(chanend c_ep0_out, chanend c_ep0_in, chanend c_audioControl,
                          *              - Audio STREAMING interface request  (In or Out)
                          *              - Audio endpoint request (Audio 1.0 Sampling freq requests are sent to the endpoint)
                          */
-                        if(((interfaceNum == 0) || (interfaceNum == 1) || (interfaceNum == 2)) && !DFU_mode_active)
-				        {
+                        if(((interfaceNum == 0) || (interfaceNum == 1) || (interfaceNum == 2)) 
+#ifdef DFU
+                                && !DFU_mode_active
+#endif
+                            )
+                        {
 #if (AUDIO_CLASS == 2) && defined(AUDIO_CLASS_FALLBACK)
                             if(g_curUsbSpeed == XUD_SPEED_HS)
                             {
