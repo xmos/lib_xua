a9df2095c6032549278c9cb0522799cb19d80226
Ross Owen <ross@xmos.com>
2016-03-17 22:45:17 +0000

    BCD_DEVICE update

--- module_usb_audio/devicedefines.h
+++ module_usb_audio/devicedefines.h
@@ -465,14 +465,14 @@
  * @brief Device firmware version number in Binary Coded Decimal format: 0xJJMN where JJ: major, M: minor, N: sub-minor version number.
  */
 #ifndef BCD_DEVICE_M
-#define BCD_DEVICE_M             15
+#define BCD_DEVICE_M             16
 #endif
 
 /**
  * @brief Device firmware version number in Binary Coded Decimal format: 0xJJMN where JJ: major, M: minor, N: sub-minor version number.
  */
 #ifndef BCD_DEVICE_N
-#define BCD_DEVICE_N             1
+#define BCD_DEVICE_N             0
 #endif
 
 /**
