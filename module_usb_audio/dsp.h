ee10a5cdf76d519d13f606f77b4fabaabe6a00c1
Ross Owen <ross@xmos.com>
2016-04-19 11:58:33 +0100

    Added dsp.h

---
+++ module_usb_audio/dsp.h
@@ -0,0 +1,27 @@
+
+
+#ifndef _DSP_H_
+#define _DSP_H_
+/* Processing interface and task */
+/* Interface to transfer block of samples to/from DSP task */
+typedef interface dsp_if
+{
+    [[guarded]]
+    void transfer_buffers(int * unsafe in_aud_buf, int * unsafe in_usb_buf,
+                            int * unsafe out_usb_buf, int * unsafe out_aud_buf);
+} dsp_if;
+
+
+/* Control interface and task */
+typedef interface dsp_ctrl_if
+{
+    int setControl(unsigned moduleId, unsigned control, unsigned setting);
+    //TODO add GetConttol
+} dsp_ctrl_if;
+
+
+void dsp_process(server dsp_if i_dsp, server dsp_ctrl_if ?i_dsp_ctrl[numDspCtrlInts], unsigned numDspCtrlInts);
+
+
+#endif
+
