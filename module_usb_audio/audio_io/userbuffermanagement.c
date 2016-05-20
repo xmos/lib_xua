1e25625106163686d38d27f69854f504189a9c87
Ross Owen <ross@xmos.com>
2016-05-20 15:04:28 +0100

    Removal of RUN_DSP_TASK

--- module_usb_audio/audio_io/userbuffermanagement.c
+++ module_usb_audio/audio_io/userbuffermanagement.c
@@ -4,14 +4,10 @@
 
 /* Default implentation for UserBufferManagement() */
 void UserBufferManagement(unsigned sampsFromUsbToAudio[], unsigned sampsFromAudioToUsb[]
-#ifdef RUN_DSP_TASK
         , unsigned i_dsp
-#endif
         ) __attribute__ ((weak));
 void UserBufferManagement(unsigned sampsFromUsbToAudio[], unsigned sampsFromAudioToUsb[]
-#ifdef RUN_DSP_TASK
         , unsigned i_dsp
-#endif
 )
 {
     /* Do nothing */
