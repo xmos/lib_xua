4956a4c67350fc12a4588053ecbee08c53634ad2
Ross Owen <ross@xmos.com>
2016-11-16 12:22:55 +0000

    Added default implementation of user_pdm_process and user_pdm_init (weak symbols)

---
+++ module_usb_audio/pdm_mics/pdm_mic_user.c
@@ -0,0 +1,24 @@
+
+#include "devicedefines.h"
+#include "mic_array_frame.h" 
+
+/* Deafult implementations of user_pdm_init() and user_pdm_process().  Both can be over-ridden */
+void user_pdm_init() __attribute__ ((weak));
+void user_pdm_init()
+{
+    return;
+}
+
+
+void user_pdm_process() __attribute__ ((weak));
+void user_pdm_process(mic_array_frame_time_domain * audio, int output[])
+{
+
+    for(unsigned i=0; i<NUM_PDM_MICS; i++)
+    {
+        /* Simply copy input buffer to output buffer unmodified */
+        output[i] = audio->data[i][0];
+    }
+
+    return;
+}
