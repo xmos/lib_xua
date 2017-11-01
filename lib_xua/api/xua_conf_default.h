8ceec63c5de801ea4456c7849c061b9df5825316
xross <ross@xmos.com>
2017-11-01 16:06:25 +0000

    Set default BCD_DEVICE to 1.0.0

--- lib_xua/api/xua_conf_default.h
+++ lib_xua/api/xua_conf_default.h
@@ -521,21 +521,21 @@
  * @brief Device firmware version number in Binary Coded Decimal format: 0xJJMN where JJ: major, M: minor, N: sub-minor version number.
  */
 #ifndef BCD_DEVICE_J
-#define BCD_DEVICE_J             7
+#define BCD_DEVICE_J             1
 #endif
 
 /**
  * @brief Device firmware version number in Binary Coded Decimal format: 0xJJMN where JJ: major, M: minor, N: sub-minor version number.
  */
 #ifndef BCD_DEVICE_M
-#define BCD_DEVICE_M             4
+#define BCD_DEVICE_M             0
 #endif
 
 /**
  * @brief Device firmware version number in Binary Coded Decimal format: 0xJJMN where JJ: major, M: minor, N: sub-minor version number.
  */
 #ifndef BCD_DEVICE_N
-#define BCD_DEVICE_N             1
+#define BCD_DEVICE_N             0
 #endif
 
 /**
