3fc2729db853ae4ba8e24c11d25740b09f1130bd
Oscar Bailey <oscarbailey@xmos.com>
2019-08-23 09:52:53 +0100

    Update Copyright

--- lib_xua/src/core/user/audiostream/audiostream.c
+++ lib_xua/src/core/user/audiostream/audiostream.c
@@ -1,4 +1,4 @@
-// Copyright (c) 2013-2018, XMOS Ltd, All rights reserved
+// Copyright (c) 2013-2019, XMOS Ltd, All rights reserved
 
 /* Default implementations of AudioStreamStop() and AudioStreamStart()
  * callbacks.
--- lib_xua/src/core/user/audiostream/audiostream.h
+++ lib_xua/src/core/user/audiostream/audiostream.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011-2018, XMOS Ltd, All rights reserved
+// Copyright (c) 2011-2019, XMOS Ltd, All rights reserved
 #ifndef _AUDIOSTREAM_H_
 #define _AUDIOSTREAM_H_
 
