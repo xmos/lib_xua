bef8fad109d226c8a6565ed1af212243fbb50f55
lucianom <lucianom@xmos.com>
2020-02-07 19:17:24 +0000

    Add new API functions for VID and PID

--- lib_xua/api/xua_endpoint0.h
+++ lib_xua/api/xua_endpoint0.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011-2018, XMOS Ltd, All rights reserved
+// Copyright (c) 2011-2020, XMOS Ltd, All rights reserved
 
 #ifndef _XUA_ENDPOINT0_H_
 #define _XUA_ENDPOINT0_H_
@@ -27,6 +27,30 @@
 void XUA_Endpoint0(chanend c_ep0_out, chanend c_ep0_in, chanend c_audioCtrl,
         chanend ?c_mix_ctl,chanend ?c_clk_ctl, chanend ?c_EANativeTransport_ctr, client interface i_dfu ?dfuInterface
          VENDOR_REQUESTS_PARAMS_DEC_);
+
+/** Function to set the Vendor ID value
+ *
+ *  \param vid vendor ID value to set
+*/
+void XUA_Endpoint0_setVendorId(unsigned short vid);
+
+/** Function to set the Product ID value
+ *
+ *  \param pid Product ID value to set
+*/
+void XUA_Endpoint0_setProductId(unsigned short pid);
+
+/** Function to get the Vendor ID value
+ *
+ *  \return Vendor ID value
+*/
+unsigned short XUA_Endpoint0_getVendorId();
+
+/** Function to get the Vendor ID value
+ *
+ *  \return Product ID value
+*/
+unsigned short XUA_Endpoint0_getProductId();
 
 #endif
 #endif
