dbaf66fdb286e8d35a49fb9ae6d604ed3bae61e3
Keith Au <keithau@xmos.com>
2020-02-28 17:08:58 +0800

    removed #if XC check

--- lib_xua/api/xua_api.h
+++ lib_xua/api/xua_api.h
@@ -3,12 +3,10 @@
 #ifndef __XUA_API_H__
 #define __XUA_API_H__
 
-#if __XC__
 extern void set_usb_to_device_rate(uint32_t rate);
 extern void set_device_to_usb_rate(uint32_t rate);
 extern void set_usb_to_device_bit_res(uint32_t rate);
 extern void set_device_to_usb_bit_res(uint32_t rate);
-#endif
 
 extern uint32_t get_usb_to_device_rate();
 extern uint32_t get_device_to_usb_rate();
