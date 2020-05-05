801e3fbe4e9fdd22ca1a12ebb9944d01cbba2053
Ross Owen <ross@xmos.com>
2020-05-05 16:37:17 +0100

    Update interrupt.h

--- lib_xua/src/core/buffer/decouple/interrupt.h
+++ lib_xua/src/core/buffer/decouple/interrupt.h
@@ -71,7 +71,7 @@
 
 //int ksp_enter, ksp_exit, r11_store;
 
-#ifndef __XS1A__
+#if defined(__XS2A__) || defined(__XS3A__)
 #define ISSUE_MODE_SINGLE ".issue_mode single\n"
 #define ISSUE_MODE_DUAL ".issue_mode dual\n"
 #else
