bd805acf04477e4be709df97334550fa867d96f9
mbanth <mbanth@animus-opus.com>
2021-05-19 17:06:58 +0100

    Correct headers

--- tests/xua_unit_tests/src/xua_unit_tests.h
+++ tests/xua_unit_tests/src/xua_unit_tests.h
@@ -1,25 +1,17 @@
-// Copyright 2018-2021 XMOS LIMITED.
+// Copyright 2021 XMOS LIMITED.
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
-#ifndef VTB_UNIT_TESTS_H_
-#define VTB_UNIT_TESTS_H_
+#ifndef XUA_UNIT_TESTS_H_
+#define XUA_UNIT_TESTS_H_
 
 #include "unity.h"
 
 #ifdef __XC__
 
 #include <xs1.h>
-#include <string.h>
-#include <math.h>
-
 #include <xclib.h>
 
-#include "audio_test_tools.h"
-#include "voice_toolbox.h"
-#include "voice_toolbox_fp.h"
-#include "vtb_references.h"
-
-#define TEST_ASM 1
+#include "xua_conf.h"
 
 #endif // __XC__
 
-#endif /* VTB_UNIT_TESTS_H_ */
+#endif /* XUA_UNIT_TESTS_H_ */
