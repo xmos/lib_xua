4033b26587bcc77be2844197b651f578018f8999
Ed <ed@xmos.com>
2021-07-06 15:13:55 +0100

    Update API with note about nullable c_audioCtrl is passed

--- lib_xua/api/xua_endpoint0.h
+++ lib_xua/api/xua_endpoint0.h
@@ -15,7 +15,10 @@
  *  \param c_ep0_out Chanend connected to the XUD_Manager() out endpoint array
  *  \param c_ep0_in Chanend connected to the XUD_Manager() in endpoint array
  *  \param c_audioCtrl Chanend connected to the decouple thread for control
- *                     audio (sample rate changes etc.)
+ *                     audio (sample rate changes etc.). Note when nulled, the
+ *                    audio device only supports single sample rate/format and
+ *                    DFU is not supported either since this channel is used
+ *                    to carry messages about format, rate and DFU state
  *  \param c_mix_ctl Optional chanend to be connected to the mixer thread if
  *                   present
  *  \param c_clk_ctl Optional chanend to be connected to the clockgen thread if
