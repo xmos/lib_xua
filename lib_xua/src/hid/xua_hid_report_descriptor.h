145c0cb80f59a17630a3189432ab803e38198b19
mbanth <mbanth@animus-opus.com>
2021-11-26 10:46:03 +0000

    Set constant values explicitly to unsigned

--- lib_xua/src/hid/xua_hid_report_descriptor.h
+++ lib_xua/src/hid/xua_hid_report_descriptor.h
@@ -19,30 +19,30 @@
 #include <stddef.h>
 
 #define HID_REPORT_ITEM_HDR_SIZE_MASK       ( 0x03 )
-#define HID_REPORT_ITEM_HDR_SIZE_SHIFT      ( 0 )
+#define HID_REPORT_ITEM_HDR_SIZE_SHIFT      ( 0U )
 
 #define HID_REPORT_ITEM_HDR_TAG_MASK        ( 0xF0 )
-#define HID_REPORT_ITEM_HDR_TAG_SHIFT       ( 4 )
+#define HID_REPORT_ITEM_HDR_TAG_SHIFT       ( 4U )
 
 #define HID_REPORT_ITEM_HDR_TYPE_MASK       ( 0x0C )
-#define HID_REPORT_ITEM_HDR_TYPE_SHIFT      ( 2 )
+#define HID_REPORT_ITEM_HDR_TYPE_SHIFT      ( 2U )
 
 #define HID_REPORT_ELEMENT_LOC_BIT_MASK     ( 0x0070 )
-#define HID_REPORT_ELEMENT_LOC_BIT_SHIFT    ( 4 )
+#define HID_REPORT_ELEMENT_LOC_BIT_SHIFT    ( 4U )
 
 #define HID_REPORT_ELEMENT_LOC_BYTE_MASK    ( 0x000F )
-#define HID_REPORT_ELEMENT_LOC_BYTE_SHIFT   ( 0 )
+#define HID_REPORT_ELEMENT_LOC_BYTE_SHIFT   ( 0U )
 
 #define HID_REPORT_ELEMENT_LOC_ID_MASK      ( 0xF000 )
-#define HID_REPORT_ELEMENT_LOC_ID_SHIFT     ( 12 )
+#define HID_REPORT_ELEMENT_LOC_ID_SHIFT     ( 12U )
 
 #define HID_REPORT_ELEMENT_LOC_LEN_MASK     ( 0x0F00 )
-#define HID_REPORT_ELEMENT_LOC_LEN_SHIFT    ( 8 )
+#define HID_REPORT_ELEMENT_LOC_LEN_SHIFT    ( 8U )
 
-#define HID_REPORT_ITEM_MAX_SIZE            ( 2 )
+#define HID_REPORT_ITEM_MAX_SIZE            ( 2U )
 
-#define HID_REPORT_ITEM_USAGE_TAG           ( 0 )
-#define HID_REPORT_ITEM_USAGE_TYPE          ( 2 )
+#define HID_REPORT_ITEM_USAGE_TAG           ( 0U )
+#define HID_REPORT_ITEM_USAGE_TYPE          ( 2U )
 
 #define HID_REPORT_SET_LOC(id, len, byte, bit) (\
     ((   id << HID_REPORT_ELEMENT_LOC_ID_SHIFT   ) & HID_REPORT_ELEMENT_LOC_ID_MASK   ) | \
@@ -50,12 +50,12 @@
     (( byte << HID_REPORT_ELEMENT_LOC_BYTE_SHIFT ) & HID_REPORT_ELEMENT_LOC_BYTE_MASK ) | \
     ((  bit << HID_REPORT_ELEMENT_LOC_BIT_SHIFT  ) & HID_REPORT_ELEMENT_LOC_BIT_MASK  ))
 
-#define HID_STATUS_GOOD         ( 0 )
-#define HID_STATUS_BAD_HEADER   ( 1 )
-#define HID_STATUS_BAD_ID       ( 2 )
-#define HID_STATUS_BAD_LOCATION ( 3 )
-#define HID_STATUS_BAD_PAGE     ( 4 )
-#define HID_STATUS_IN_USE       ( 5 )
+#define HID_STATUS_GOOD         ( 0U )
+#define HID_STATUS_BAD_HEADER   ( 1U )
+#define HID_STATUS_BAD_ID       ( 2U )
+#define HID_STATUS_BAD_LOCATION ( 3U )
+#define HID_STATUS_BAD_PAGE     ( 4U )
+#define HID_STATUS_IN_USE       ( 5U )
 
 /**
  * @brief USB HID Report Descriptor Short Item
