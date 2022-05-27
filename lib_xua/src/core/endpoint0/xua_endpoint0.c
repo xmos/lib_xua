805c5fca9c56b5aa0ea3b693de62c808aaff5b4f
lucianom <lucianom@xmos.com>
2022-05-27 13:53:16 +0100

    Update copyright date

--- lib_xua/src/core/endpoint0/xua_endpoint0.c
+++ lib_xua/src/core/endpoint0/xua_endpoint0.c
@@ -1,4 +1,4 @@
-// Copyright 2011-2021 XMOS LIMITED.
+// Copyright 2011-2022 XMOS LIMITED.
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
 /**
  * @brief   Implements endpoint zero for an USB Audio 1.0/2.0 device
