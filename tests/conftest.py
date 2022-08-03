43c561c831321b679becfd0489fb51ee0c404428
xross <ross@xmos.com>
2022-08-03 15:12:12 +0100

    Add tracing related options to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,19 @@ import pytest
 
 
 def pytest_addoption(parser):
-    pass
-
+    parser.addoption(
+        "--enabletracing",
+        action="store_true",
+        default=False,
+        help="Run tests with instruction tracing",
+    )
+ 
+    parser.addoption(
+        "--enablevcdtracing",
+        action="store_true",
+        default=False,
+        help="Run tests with vcd tracing",
+    )
 
 @pytest.fixture
 def options(request):
