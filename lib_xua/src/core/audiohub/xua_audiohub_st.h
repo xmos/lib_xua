0ce91bec906604f5da31d28b0575ffe386684df4
Ross Owen <ross@xmos.com>
2023-01-17 12:12:46 +0000

    Copyright comment

--- lib_xua/src/core/audiohub/xua_audiohub_st.h
+++ lib_xua/src/core/audiohub/xua_audiohub_st.h
@@ -1,7 +1,6 @@
 // Copyright 2011-2023 XMOS LIMITED.
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
 
-
 #pragma unsafe arrays
 static inline unsigned DoSampleTransfer(chanend ?c_out, const int readBuffNo, const unsigned underflowWord)
 {
