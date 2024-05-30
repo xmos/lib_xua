aef5311a4b2963ec83598ea3348735bb1d23a32c
Ross Owen <ross@xmos.com>
2024-05-30 14:02:08 +0100

    Pin lib_sw_pll to v2.2.0

--- lib_xua/lib_build_info.cmake
+++ lib_xua/lib_build_info.cmake
@@ -25,7 +25,7 @@ set(LIB_DEPENDENT_MODULES "lib_adat(1.1.0)"
                           "lib_logging(3.2.0)"
                           "lib_mic_array(4.6.0)"
                           "lib_spdif(6.1.0)"
-                          "lib_sw_pll(develop)"
+                          "lib_sw_pll(2.2.0)"
                           "lib_xassert(4.2.0)"
                           "lib_xud(2.3.1)")
 
