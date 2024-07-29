2a6690a53ef23a8211730d23dd092ce3d28c18f9
Ross Owen <ross@xmos.com>
2024-07-29 16:49:12 +0100

    Bump dep lib_spdif 6.1.0 -> 6.1.1

--- lib_xua/lib_build_info.cmake
+++ lib_xua/lib_build_info.cmake
@@ -24,7 +24,7 @@ set(LIB_DEPENDENT_MODULES "lib_adat(1.2.0)"
                           "lib_locks(2.2.0)"
                           "lib_logging(3.2.0)"
                           "lib_mic_array(4.6.0)"
-                          "lib_spdif(6.1.0)"
+                          "lib_spdif(6.1.1)"
                           "lib_sw_pll(2.2.0)"
                           "lib_xassert(4.2.0)"
                           "lib_xud(2.3.2)")
