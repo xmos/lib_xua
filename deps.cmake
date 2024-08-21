07f58d161433f21f5671bd704a24a70500e0c8e0
Daniel Pieczko <danielpieczko@xmos.com>
2024-08-21 11:35:42 +0100

    Convert examples to use XCommon CMake

---
+++ deps.cmake
@@ -0,0 +1,6 @@
+set(APP_DEPENDENT_MODULES "lib_xua"
+                          "lib_xud(2.3.2)"
+                          "lib_i2c(6.2.0)"
+                          "lib_spdif(6.1.1)"
+                          "lib_mic_array(4.6.0)"
+                          )
