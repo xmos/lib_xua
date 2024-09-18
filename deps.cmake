1e7fd6be9182632ec7dc80da6e3a89900e6494cc
Ed <ed@xmos.com>
2024-09-18 16:07:07 +0100

    Use latest mic array in deps

--- deps.cmake
+++ deps.cmake
@@ -3,4 +3,5 @@ set(APP_DEPENDENT_MODULES "lib_xua"
                           "lib_i2c(6.2.0)"
                           "lib_spdif(6.1.1)"
                           "lib_mic_array(4.6.0)"
+                          "lib_board_support(1.0.0)"
                           )
