b22b21fcc9f86547e3a51b943b9cf946221b9d96
Ed <ed@xmos.com>
2024-09-20 08:35:07 +0100

    Source check

--- lib_xua/src/core/pdm_mics/mic_array_task.c
+++ lib_xua/src/core/pdm_mics/mic_array_task.c
@@ -1,5 +1,5 @@
-// Copyright 2022-2023 XMOS LIMITED.
-// This Software is subject to the terms of the XCORE VocalFusion Licence.
+// Copyright 2022-2024 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public Licence: Version 1.
 
 #include "xua_conf.h"
 #if (XUA_NUM_PDM_MICS > 0)
