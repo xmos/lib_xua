60963d91d5d87784e896a7acf6fd9c9f2e603c0e
Ed <ed@xmos.com>
2024-09-26 18:10:21 +0100

    Update apps for MA

--- examples/AN00246_xua_example/src/xua_conf.h
+++ examples/AN00246_xua_example/src/xua_conf.h
@@ -22,6 +22,5 @@
 #define PID_AUDIO_1     1
 #define PID_AUDIO_2     2
 #define XUA_DFU_EN      0           /* Disable DFU (for simplicity of example */
-#define MIC_DUAL_ENABLED 0          // Use multi-threaded design
 
 #endif
