1b0e64cfc058c6b07acaaf778f8855c448d5f5ec
Ed <ed@xmos.com>
2024-09-26 17:48:00 +0100

    Cleanup defines

--- lib_xua/api/xua_pdm_mic.h
+++ lib_xua/api/xua_pdm_mic.h
@@ -4,30 +4,10 @@
 #ifndef XUA_PDM_MIC_H
 #define XUA_PDM_MIC_H
 
-#include "xua_conf_full.h"
-
-#define MIC_ARRAY_CONFIG_MCLK_FREQ          MCLK_48
-#define MIC_ARRAY_CONFIG_PDM_FREQ           3072000
-#define MIC_ARRAY_CONFIG_SAMPLES_PER_FRAME  1
-#define MIC_ARRAY_CONFIG_PORT_MCLK          XS1_PORT_1D
-#define MIC_ARRAY_CONFIG_PORT_PDM_CLK       XS1_PORT_1G
-#define MIC_ARRAY_CONFIG_PORT_PDM_DATA      XS1_PORT_1F
-#define MIC_ARRAY_CONFIG_USE_DDR            1
-#define MIC_ARRAY_CONFIG_CLOCK_BLOCK_A      XS1_CLKBLK_1      
-#define MIC_ARRAY_CONFIG_CLOCK_BLOCK_B      XS1_CLKBLK_2
-#define appconfSHF_NOMINAL_HZ               MIN_FREQ
-#define MIC_ARRAY_CONFIG_MIC_COUNT          XUA_NUM_PDM_MICS
-#define MIC_ARRAY_CONFIG_MIC_IN_COUNT       XUA_NUM_PDM_MICS
-#define MIC_ARRAY_CONFIG_USE_DC_ELIMINATION 1
-
-#if (MIC_ARRAY_CONFIG_SAMPLES_PER_FRAME != 1)
-#error ONLY MIC_ARRAY_CONFIG_SAMPLES_PER_FRAME = 1 supported currently
-#endif
-
-/* Included from lib_mic_array */
 #include <xccompat.h>
-
 #include <stdint.h>
+
+#include "mic_array_conf.h"
 #include "mic_array.h"
 
 #ifdef __cplusplus
--- lib_xua/src/core/pdm_mics/mic_array_conf.h
+++ lib_xua/src/core/pdm_mics/mic_array_conf.h
@@ -1,27 +1,53 @@
-// Copyright 2015-2021 XMOS LIMITED.
+// Copyright 2015-2024 XMOS LIMITED.
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
 #ifndef MIC_ARRAY_CONF_H_
 #define MIC_ARRAY_CONF_H_
 
 #include "xua_conf_full.h"
 
-// The default definition of MIC_ARRAY_MAX_FRAME_SIZE_LOG2 must appear in this file
-//   because an assembler file #includes it.
-// The assembler does not understand C language syntax, only C pre-processor syntax.
-#ifndef MIC_ARRAY_MAX_FRAME_SIZE_LOG2
-#define MIC_ARRAY_MAX_FRAME_SIZE_LOG2 (0)
+/* PDM mics configuration */
+#ifndef MIC_ARRAY_CONFIG_MCLK_FREQ
+#define MIC_ARRAY_CONFIG_MCLK_FREQ          MCLK_48     // Used with MIC_ARRAY_CONFIG_PDM_FREQ to calculate clock divider 
+#endif
+#ifndef MIC_ARRAY_CONFIG_PDM_FREQ
+#define MIC_ARRAY_CONFIG_PDM_FREQ           3072000     // Used with MIC_ARRAY_CONFIG_MCLK_FREQ to calculate clock divider 
+#endif
+#ifndef MIC_ARRAY_CONFIG_USE_DC_ELIMINATION
+#define MIC_ARRAY_CONFIG_USE_DC_ELIMINATION 1           // DC filter
+#endif
+#ifndef MIC_ARRAY_CONFIG_USE_DDR
+#define MIC_ARRAY_CONFIG_USE_DDR            1           // Two mics on one data line by default
+#endif
+#ifndef MIC_ARRAY_CONFIG_SAMPLES_PER_FRAME
+#define MIC_ARRAY_CONFIG_SAMPLES_PER_FRAME  1           // XUA is sample based
+#endif
+#define MIC_ARRAY_CONFIG_MIC_COUNT          XUA_NUM_PDM_MICS
+#define MIC_ARRAY_CONFIG_MIC_IN_COUNT       XUA_NUM_PDM_MICS
+
+/* PDM mics resources */
+#ifndef MIC_ARRAY_CONFIG_PORT_MCLK
+#error Please define MIC_ARRAY_CONFIG_PORT_MCLK for PDM mics
+#endif
+#ifndef MIC_ARRAY_CONFIG_PORT_PDM_CLK
+#error Please define MIC_ARRAY_CONFIG_PORT_PDM_CLK for PDM mics
+#endif
+#ifndef MIC_ARRAY_CONFIG_PORT_PDM_CLK
+#error Please define MIC_ARRAY_CONFIG_PORT_PDM_CLK for PDM mics
+#endif
+#ifndef MIC_ARRAY_CONFIG_PORT_PDM_CLK
+#error Please define MIC_ARRAY_CONFIG_PORT_PDM_CLK for PDM mics
+#endif
+#ifndef MIC_ARRAY_CONFIG_PORT_PDM_CLK
+#error Please define MIC_ARRAY_CONFIG_PORT_PDM_CLK for PDM mics
 #endif
 
-#ifndef MIC_ARRAY_NUM_MICS
-#define MIC_ARRAY_NUM_MICS (XUA_NUM_PDM_MICS)
+/* Test for errors in config */
+#ifndef XUA_SAMP_FREQ
+#error Please define XUA_SAMP_FREQ when using PDM mics (single sample rate only supported currently)
 #endif
 
-// MIC_DUAL_FRAME_SIZE has no meaning if MIC_DUAL_ENABLED is false.
-// Only define MIC_DUAL_FRAME_SIZE if MIC_DUAL_ENABLED is true.
-#if defined(MIC_DUAL_ENABLED) && (MIC_DUAL_ENABLED != 0)
-#ifndef MIC_DUAL_FRAME_SIZE
-#define MIC_DUAL_FRAME_SIZE (XUA_MIC_FRAME_SIZE)
-#endif
+#if (MIC_ARRAY_CONFIG_SAMPLES_PER_FRAME != 1)
+#error ONLY MIC_ARRAY_CONFIG_SAMPLES_PER_FRAME = 1 supported currently
 #endif
 
 #endif /* MIC_ARRAY_CONF_H_ */
