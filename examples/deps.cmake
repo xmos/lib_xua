d11852390505adb14654a43f46709287c49e288f
Ed <ed@xmos.com>
2024-10-01 15:05:56 +0100

    use mic_array develop

--- examples/deps.cmake
+++ examples/deps.cmake
@@ -1,4 +1,4 @@
 set(APP_DEPENDENT_MODULES "lib_xua"
                           "lib_board_support(1.0.1)"
-                          "lib_mic_array(5.3.0)"
+                          "lib_mic_array(develop)"
                           )
