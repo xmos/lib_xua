2e7b44aac7c7652279506ec9aaa8cf527d5fc73e
Shuchita Khare <shuchitak@xmos.com>
2024-10-30 11:07:12 +0000

    Update dependency versions

--- tests/xua_sim_tests/test_deps.cmake
+++ tests/xua_sim_tests/test_deps.cmake
@@ -1,3 +1,3 @@
-list(APPEND APP_DEPENDENT_MODULES "lib_logging(3.2.0)"
-                                  "lib_random"
-                                  "lib_unity(2.5.2)")
+list(APPEND APP_DEPENDENT_MODULES "lib_logging(3.3.1)"
+                                  "lib_random(1.2.0)"
+                                  "lib_unity(2.6.1)")
