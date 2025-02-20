f8b4dbeae57bde8c7e9236a7137cb85e34eb5b94
Ed <ed@xmos.com>
2025-02-20 15:00:56 +0000

    copyright update to 2025

--- lib_xua/src/core/audiohub/audiohub_dsd.h
+++ lib_xua/src/core/audiohub/audiohub_dsd.h
@@ -1,4 +1,4 @@
-// Copyright 2018-2024 XMOS LIMITED.
+// Copyright 2018-2025 XMOS LIMITED.
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
 
 #if (DSD_CHANS_DAC != 0)
