5d2878bd4caf6b2f36bd0764eda3d74ecaab3b48
Henk Muller <henk@xmos.com>
2025-04-10 15:05:24 +0100

    Default to negate

--- lib_xua/src/core/pwm_thread.c
+++ lib_xua/src/core/pwm_thread.c
@@ -37,7 +37,7 @@ void pwm_thread(chanend_t c_data)
                          2.8544, 2.8684735298,     // scale, limit
                          1.0/120000, -1.0/250000,  // flat_comp_x2, x3
                          3.0/157, 0.63/157,       // pwm comp x2, x3
-                         0);                       // negate
+                         1);                       // negate
 
     software_dac_hp(&sd, c_data);
 }
