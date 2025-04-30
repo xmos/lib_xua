7bd357e0f08e5f8d85e201a942b9320e857e4c05
Ed <ed@xmos.com>
2025-04-30 12:11:31 +0100

    Remove chip power down scaling which breaks DFU test and renable i0o2_lp

--- tests/xua_hw_tests/test_dfu/src/user_main.h
+++ tests/xua_hw_tests/test_dfu/src/user_main.h
@@ -11,28 +11,13 @@
 
 extern unsafe client interface i2c_master_if i_i2c_client;
 extern void board_setup();
-extern void power_down();
-
-#ifdef LOW_POWER_ENABLE
-/* Call the clock power down code as per https://github.com/xmosnotes/an01009
-*/
-#define POWER_DOWN() power_down()
-#else
-/* Do nothing */
-#define POWER_DOWN()
-#endif
 
 #define USER_MAIN_DECLARATIONS \
     interface i2c_master_if i2c[1];
 
 #define USER_MAIN_CORES on tile[0]: {\
-                                        par{\
-                                            {\
-                                                board_setup();\
-                                                xk_audio_316_mc_ab_i2c_master(i2c);\
-                                            }\
-                                            POWER_DOWN();\
-                                        }\
+                                        board_setup();\
+                                        xk_audio_316_mc_ab_i2c_master(i2c);\
                                     }\
                         on tile[1]: {\
                                         unsafe\
