8ae5fe450f0703ec615790033dec3763d9576ab4
Ed <ed@xmos.com>
2025-06-21 10:20:10 +0100

    Docs and add XUA_wrapper_get_stream_format

--- lib_xua/api/xua_wrapper.h
+++ lib_xua/api/xua_wrapper.h
@@ -7,54 +7,52 @@
 #include <stdint.h>
 #include "xccompat.h"
 
-/** USB Audio Buffering Core(s).
+/** USB Audio Wrapper subsystem.
  *
- *  This function buffers USB audio data between the XUD and the audio subsystem.
- *  Most of the chanend parameters to the function should be connected to
- *  XUD_Manager().  The uses two cores.
+ *  This task starts four threads and provides a complete USB audio subsyetem which may be
+ *  treated as simple audio source sink. It starts four logcal tasks for XUD, Endpoint 0 and
+ *  the two buffering tasks. Basic host audio input/output only is supported and I2S or any 
+ *  digital interfaces must be started and managed by the user.
+ * 
+ *  It is necessary to have a valid xua_conf.h and the user must connect the XUA internally
+ *  declared MCLK count port `p_for_mclk_count` to a clock block which is being clocked at 
+ *  the MCLK rate.
+ * 
+ *  Once this task has been started then the user need only call XUA_wrapper_exchange_samples()
+ *  at the sample rate (note this must be locked to the master clock). At startup or after a
+ *  host initiated sample or stream format change, the function XUA_wrapper_get_stream_format()
+ *  may be called to determine the host-selected audio stream format.
  *
- *  \param c_aud_out            Audio OUT endpoint channel connected to the XUD
- *  \param c_aud_in             Audio IN endpoint channel connected to the XUD
- *  \param c_aud_fb             Audio feedback endpoint channel connected to the XUD
- *  \param c_midi_from_host     MIDI OUT endpoint channel connected to the XUD
- *  \param c_midi_to_host       MIDI IN endpoint channel connected to the XUD
- *  \param c_midi               Channel connected to MIDI thread
- *  \param c_int                Audio clocking interrupt endpoint channel connected to the XUD
- *  \param c_clk_int            Optional chanend connected to the clockGen() thread if present
- *  \param c_sof                Start of frame channel connected to the XUD
- *  \param c_aud_ctl            Audio control channel connected to  Endpoint0()
- *  \param p_off_mclk           A port that is clocked of the MCLK input (not the MCLK input itself)
- *  \param c_hid                Channel connected to the HID handler thread
- *  \param c_aud                Channel connected to XUA_AudioHub() thread
- *  \param c_audio_rate_change  Channel to notify and synchronise on audio rate change
- *  \param i_pll_ref            Interface to task that toggles reference pin to CS2100
- *  \param c_swpll_update       Channel connected to software PLL task. Expects master clock counts based on USB frames.
+ *  \param c_aud                Channel connected to the block which exchanges audio with USB
  */
-
 void XUA_wrapper_task(chanend c_aud);
 
-/** USB Audio Buffering Core(s).
+/** Exchange samples with XUA_wrapper_task()
  *
  *  This function buffers USB audio data between the XUD and the audio subsystem.
  *  Most of the chanend parameters to the function should be connected to
  *  XUD_Manager().  The uses two cores.
  *
- *  \param c_aud_out            Audio OUT endpoint channel connected to the XUD
- *  \param c_aud_in             Audio IN endpoint channel connected to the XUD
- *  \param c_aud_fb             Audio feedback endpoint channel connected to the XUD
- *  \param c_midi_from_host     MIDI OUT endpoint channel connected to the XUD
- *  \param c_midi_to_host       MIDI IN endpoint channel connected to the XUD
- *  \param c_midi               Channel connected to MIDI thread
- *  \param c_int                Audio clocking interrupt endpoint channel connected to the XUD
- *  \param c_clk_int            Optional chanend connected to the clockGen() thread if present
- *  \param c_sof                Start of frame channel connected to the XUD
- *  \param c_aud_ctl            Audio control channel connected to  Endpoint0()
- *  \param p_off_mclk           A port that is clocked of the MCLK input (not the MCLK input itself)
- *  \param c_hid                Channel connected to the HID handler thread
- *  \param c_aud                Channel connected to XUA_AudioHub() thread
- *  \param c_audio_rate_change  Channel to notify and synchronise on audio rate change
- *  \param i_pll_ref            Interface to task that toggles reference pin to CS2100
- *  \param c_swpll_update       Channel connected to software PLL task. Expects master clock counts based on USB frames.
+ *  \param c_aud                Channel connected to XUA_wrapper_task()
+ *  \param samples_to_host      An array of samples which are to be passed to the host (input)
+ *  \param samples_from_host    An array of samples which have been provided by the host (output)
+ * 
+ *  \returns                    Zero if samples have been exchanged normally, 1 if a stream format
+ *                              change has been requested by the host
  */
-void XUA_wrapper_exchange_samples(chanend c_aud, int32_t samples_to_host[NUM_USB_CHAN_IN], int32_t samples_from_host[NUM_USB_CHAN_OUT]);
+int XUA_wrapper_exchange_samples(chanend c_aud, int32_t samples_to_host[NUM_USB_CHAN_IN], int32_t samples_from_host[NUM_USB_CHAN_OUT]);
+
+/** Get the latest stream format requested by host to XUA_wrapper_task()
+ *
+ *  This function provides an way of querying the current host audio streaming settings.
+ *
+ *  \param curSamFreq       Pointer to sample rate in Hertz
+ *  \param mClk             Reference to Master clock rate in Hertz
+ *  \param curSamRes_DAC    Reference to output stream bit resolution
+ *  \param curSamRes_ADC    Reference Input stream bit resolution
+ * 
+ */
+void XUA_wrapper_get_stream_format(unsigned *curSamFreq, unsigned *mClk, unsigned *curSamRes_DAC, unsigned *curSamRes_ADC);
+
+
 #endif
--- lib_xua/src/core/xua_wrapper.xc
+++ lib_xua/src/core/xua_wrapper.xc
@@ -1,34 +1,61 @@
 // Copyright 2025 XMOS LIMITED.
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
 
-#include "xua.h"                          /* Device specific defines */
+#include "xua.h"
 #include "xua_commands.h"
 #include <string.h>
 
 #define XUA_MAX(x,y) ((x)>(y) ? (x) : (y))
 
-
-const int readBuffNo = 0;
-unsigned command = XUA_AUDCTL_NO_COMMAND;
-unsigned underflowWord = 0;
+const int readBuffNo = 0; 	// Note we don't need to double buffer here due to copy
+unsigned underflowWord = 0;	// Always assumuing PCM
+// Variables used by audiohub for sample transfer 
 extern unsigned samplesOut[XUA_MAX(NUM_USB_CHAN_OUT, I2S_CHANS_DAC)];
 extern unsigned samplesIn[2][XUA_MAX(NUM_USB_CHAN_IN, I2S_CHANS_DAC)];
+
+/* Sample transfer functions use on the audiohub side */
+#include "xua_audiohub_st.h"
 extern void receive_command(unsigned command, chanend c_aud, unsigned &curSamFreq, unsigned &dsdMode, unsigned &curSamRes_DAC, unsigned &audioActive);
 
-#include "xua_audiohub_st.h"
+// Setup to initial defaults
+unsigned command = XUA_AUDCTL_NO_COMMAND;
+unsigned xua_wrapper_sample_rate = DEFAULT_FREQ; 							// MIN_FREQ
+unsigned xua_wrapper_mclk_rate = DEFAULT_MCLK;   							// Choose correct MCLK for DEFAULT_FREQ
+unsigned xua_wrapper_dsd_mode = 0;											// Not in DSD mode
+unsigned xua_wrapper_dac_res = STREAM_FORMAT_OUTPUT_1_RESOLUTION_BITS;
+unsigned xua_wrapper_adc_res = STREAM_FORMAT_INPUT_1_RESOLUTION_BITS;
 
-void XUA_wrapper_exchange_samples(chanend c_aud, int32_t samples_to_host[NUM_USB_CHAN_IN], int32_t samples_from_host[NUM_USB_CHAN_OUT])
+int XUA_wrapper_exchange_samples(chanend c_aud, int32_t samples_to_host[NUM_USB_CHAN_IN], int32_t samples_from_host[NUM_USB_CHAN_OUT])
 {
-	StartSampleTransfer(c_aud, underflowWord);                  /* Send first token to fire ISR in decouple */
+	StartSampleTransfer(c_aud, underflowWord);                  // Send first token to fire ISR in decouple
 	memcpy(samplesIn[readBuffNo], samples_to_host, sizeof(samples_to_host));
-	CompleteSampleTransferUsbChans(c_aud, readBuffNo, command); /* Check for command & transfer the samples & UBM */
+	CompleteSampleTransferUsbChans(c_aud, readBuffNo, command); // Check for command & transfer the samples & UBM
 	memcpy(samples_from_host, samplesOut, sizeof(samples_from_host));
 	if(command != XUA_AUDCTL_NO_COMMAND)
 	{
 	    /* Just consume the command, ignore it + keep on looping forever */
-	    unsigned dummy1, dummy2, dummy3, dummy4;
-	    receive_command(command, c_aud, dummy1, dummy2, dummy3, dummy4);
+	    unsigned audioActive = 1;
+	    receive_command(command, c_aud, xua_wrapper_sample_rate, xua_wrapper_dsd_mode, xua_wrapper_dac_res, audioActive);
+	    /* Calculate what master clock we should be using */
+        if (((MCLK_441) % xua_wrapper_sample_rate) == 0)
+        {
+            xua_wrapper_mclk_rate = MCLK_441;
+        }
+        else if (((MCLK_48) % xua_wrapper_sample_rate	) == 0)
+        {
+            xua_wrapper_mclk_rate = MCLK_48;
+        }
 	    outct(c_aud, XS1_CT_END);
+
+	    return 1;
 	}
-    
+    return 0;
+}
+
+void XUA_wrapper_get_stream_format(unsigned *curSamFreq, unsigned *mClk, unsigned *curSamRes_DAC, unsigned *curSamRes_ADC)
+{
+	*curSamFreq = xua_wrapper_sample_rate;
+	*mClk = xua_wrapper_mclk_rate;
+	*curSamRes_DAC = xua_wrapper_dac_res;
+	*curSamRes_ADC = xua_wrapper_adc_res;
 }
