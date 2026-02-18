8bd2f279c8ca2e8b9187753a240c402d7bd68bb7
Humphrey Bucknell <humphreybucknell@xmos.com>
2026-02-18 11:22:36 +0000

    Adding dfu_conf.h and fixing builds

---
+++ lib_xua/src/dfu_conf.h
@@ -0,0 +1,11 @@
+// Copyright 2026 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public Licence: Version 1.
+
+#ifndef DFU_CONF_H
+#define DFU_CONF_H
+
+#include "xua_conf_full.h"
+
+#define DFU_ENABLE XUA_DFU_EN
+
+#endif /* DFU_CONF_H */
